ec28a0ba34e85e22d671ce3ee6324157cb76fa16
Dmitry Abramov <dmitry.i.abramov@gmail.com>
2025-04-10 01:44:47 +0200

    Check whether format-xml changed file

--- pre_commit_hook_xml/format_xml.py
+++ pre_commit_hook_xml/format_xml.py
@@ -1,8 +1,14 @@
 from __future__ import annotations
 
 import argparse
+import hashlib
 import lxml.etree as etree
 from collections.abc import Sequence
+
+def calculate_md5(filename: str) -> str:
+    with open(filename, 'rb') as f:
+        md5_hash = hashlib.md5(f.read()).hexdigest()
+    return md5_hash
 
 def main(argv: Sequence[str] | None = None) -> int:
     parser = argparse.ArgumentParser()
@@ -11,10 +17,15 @@ def main(argv: Sequence[str] | None = None) -> int:
 
     retval = 0
     for filename in args.filenames:
+        before = calculate_md5(filename)
+
         tree = etree.parse(filename)
         tree.write(filename, pretty_print=True, encoding='UTF-8', xml_declaration=True)
-        print(f'{filename}: The file has been updated ({filename})')
-        retval = 1
+
+        after = calculate_md5(filename)
+        if before != after:
+            print(f'{filename}: The file has been changed ({filename})')
+            retval = 1
 
     return retval
 
