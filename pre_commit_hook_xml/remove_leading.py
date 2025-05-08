d25bd05013bb1f09be923dbd305cb44a94292125
Dmitry Abramov <dmitry.i.abramov@gmail.com>
2025-05-08 09:44:05 +0200

    Fix packaging

--- pre_commit_hook_xml/remove_leading.py
+++ pre_commit_hook_xml/remove_leading.py
@@ -13,10 +13,17 @@ def main(argv: Sequence[str] | None = None) -> int:
         with open(filename, 'r') as f:
             lines = f.readlines()
 
+        changed = False
         with open(filename, 'w') as f:
             for line in lines:
                 stripped = line.lstrip(' ')
                 f.write(stripped)
+                if len(stripped) < len(line):
+                    changed = True
+                    retval = 1
+                    
+        if changed:
+            print(f'{filename}: The file has been changed ({filename})')
         
     return retval
 
